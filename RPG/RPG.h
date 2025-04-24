81a4d2f0277bfd1815293313f454e44feae3e709
Denny0411 <flowerhua80325@gmail.com>
2025-04-24 13:28:17 +0800

    Update the first version of the project

---
+++ RPG/RPG.h
@@ -0,0 +1,41 @@
+﻿#ifndef RPG_H
+#define RPG_H
+#include <string>
+
+class Role
+{
+private:
+	std::string name;
+	int HP;
+	int MP;
+	int LV;
+
+public:
+	Role();
+
+	void setName(std::string newName);
+	void setHP(int newHP);
+	void setMP(int newMP);
+	void setLV(int newLV);
+
+	std::string getName();
+	int getHP();
+	int getMP();
+	int getLV();
+};
+
+class Enemy
+{
+private:
+	int HP;
+
+public:
+	Enemy();
+
+	void setHP(int newHP);
+	void setMP(int newMP);
+	int getHP();
+	int getMP();
+};
+
+#endif
